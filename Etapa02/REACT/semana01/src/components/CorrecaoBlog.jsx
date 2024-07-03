107161e0b3a563367660fd583572fa6ee4e499e2
Letícia Silva <sps.leticia@gmail.com>
2024-07-03 16:55:56 -0300

    Adicionando correções da aula do dia 0207

---
+++ Etapa02/REACT/semana01/src/components/CorrecaoBlog.jsx
@@ -0,0 +1,30 @@
+import "./CorrecaoBlog.css";
+import Postagem from "./CorrecaoPostagem.jsx";
+
+function Blog() {
+  return (
+    <section className="blog">
+      <h2>Postagens do dia</h2>
+      <Postagem
+        titulo="React é top!"
+        desc="Descrição da postagem 1"
+        linkImagem="https://picsum.photos/200"
+        legenda="Imagem aleatória"
+      />
+      <Postagem
+        titulo="Estudando CSS"
+        desc="Descrição da postagem 2"
+        linkImagem="https://picsum.photos/200"
+        legenda="Imagem aleatória"
+      />
+      <Postagem
+        titulo="Estudando JS"
+        desc="Descrição da postagem 3"
+        linkImagem="https://picsum.photos/200"
+        legenda="Imagem aleatória"
+      />
+    </section>
+  );
+}
+
+export default Blog;
---
+++ Etapa02/REACT/semana01/src/components/CorrecaoRodape.jsx
@@ -0,0 +1,14 @@
+import "./CorrecaoRodape.css";
+
+function Rodape() {
+  return (
+    // Fragment: <> </>
+    // Utilizamos o fragment para agrupar elementos filhos
+    <>
+      <hr />
+      <footer>&copy; 2015-2024 SoulCode. Todos os direitos reservados.</footer>
+    </>
+  );
+}
+
+export default Rodape;
