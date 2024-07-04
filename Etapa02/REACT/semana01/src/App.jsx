30e03309666aed3450404b0d961dee73092753b3
Letícia Silva <sps.leticia@gmail.com>
2024-07-04 15:37:38 -0300

    Adicionando aula do dia 0407 sobre listas e imagens

--- Etapa02/REACT/semana01/src/App.jsx
+++ Etapa02/REACT/semana01/src/App.jsx
@@ -3,10 +3,15 @@
 // Declarativa
 // Açúcar sintático => é uma sintaxe dentro da linguagem de programação que tem por finalidade tornar suas construções mais fáceis de serem lidas e expressas
 
-import Blog from "./components/CorrecaoBlog";
-import Navbar from "./components/Navbar";
-import Rodape from "./components/CorrecaoRodape";
+import Banner from "./components/Banner";
+import Listagem from "./components/Listagem";
+
+// import Blog from "./components/CorrecaoBlog";
+// import Navbar from "./components/Navbar";
+// import Rodape from "./components/CorrecaoRodape";
 // import Mensagem from "./components/Mensagem";
+// import CardAluno from "./components/CorrecaoCardAluno";
+// import CardProduto from "./components/CorrecaoCardProduto";
 
 // Componente principal da aplicação
 function App() {
@@ -27,10 +32,18 @@ function App() {
         paragrafo="Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, vel."
         linkImagem="https://picsum.photos/200"
       /> */}
-      <Navbar logado={true} nomeUsuario="Batata" />
+      {/* <Navbar logado={true} nomeUsuario="Batata" />
       <Navbar logado={false} />
       <Blog />
-      <Rodape />
+      <Rodape /> */}
+      {/* <CardAluno nome="José" serie="7º" media={8.5} />
+      <CardAluno nome="Igor" serie="9º" media={5.5} />
+      <CardAluno nome="Victor" serie="6º" media={7.0} /> */}
+      {/* <CardProduto nome="Celular" precoUnitario={2000} desconto={10} />
+      <CardProduto nome="Fone Wireless" precoUnitario={100} desconto={15} />
+      <CardProduto nome="Carro" precoUnitario={90000} desconto={0} /> */}
+      {/* <Listagem /> */}
+      <Banner />
     </>
   );
 }
---
+++ Etapa02/REACT/semana01/src/components/Listagem.jsx
@@ -0,0 +1,73 @@
+import CardAluno from "./CorrecaoCardAluno";
+
+const compras = [
+  "Batata palha",
+  "Batata frita pronta",
+  "Batata doce",
+  "Batata chips",
+  "Batata inglesa",
+  "Batata rústica",
+  "Batata crua",
+];
+
+const produtos = [
+  { cod: 1, nome: "Batata palha", quantidade: 300, precoUnitario: 5 },
+  { cod: 2, nome: "Batata frita pronta", quantidade: 100, precoUnitario: 10 },
+  { cod: 3, nome: "Batata doce", quantidade: 3, precoUnitario: 10 },
+  { cod: 4, nome: "Batata chips", quantidade: 20, precoUnitario: 4 },
+];
+
+const alunos = [
+  { mat: 1, nome: "Amanda", serie: "7º", media: 9.0 },
+  { mat: 2, nome: "Gabriel", serie: "8º", media: 7.0 },
+  { mat: 3, nome: "Helena", serie: "9º", media: 10 },
+  { mat: 4, nome: "Pedro", serie: "8º", media: 4.5 },
+];
+
+// Gerar componentes/elementos a partir de dados (array)
+
+function Listagem() {
+  // Cada string no array de compras está sendo transformada
+  // em um array de <li>
+  const itensCompra = compras.map((compra, index) => {
+    return <li key={index}>{compra}</li>;
+  });
+
+  // O key é importante para que o React saiba remover
+  // ou adicionar o elemento na tela corretamente
+  const cardsProduto = produtos.map((produto) => {
+    return (
+      <article key={produto.cod}>
+        <h3>NOME: {produto.nome}</h3>
+        <p>QUANTIDADE: {produto.quantidade}</p>
+        <p>SUBTOTAL: {produto.quantidade * produto.precoUnitario}</p>
+      </article>
+    );
+  });
+
+  const cardsAluno = alunos.map((aluno) => {
+    return (
+      <CardAluno
+        key={aluno.mat}
+        nome={aluno.nome}
+        serie={aluno.serie}
+        media={aluno.media}
+      />
+    );
+  });
+
+  return (
+    <>
+      <h2>Lista de Compras</h2>
+      <ul>{itensCompra}</ul>
+      <hr />
+      <h2>Lista de Produtos</h2>
+      <section>{cardsProduto}</section>
+      <hr />
+      <h2>Lista de Alunos</h2>
+      <section>{cardsAluno}</section>
+    </>
+  );
+}
+
+export default Listagem;
