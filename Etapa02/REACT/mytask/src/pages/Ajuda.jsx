2792fe2d4297792863170a2e0ac2cbe2ab37d0e0
lettycodes <sps.leticia@gmail.com>
2024-07-10 14:28:42 -0300

    Adicionando projeto mytask

---
+++ Etapa02/REACT/mytask/src/pages/Ajuda.jsx
@@ -0,0 +1,37 @@
+import { Container, Accordion, Button, Alert } from "react-bootstrap";
+
+function Ajuda() {
+  return (
+    <main>
+      <Container>
+        <h1>Ajuda</h1>
+        <hr />
+        <Accordion defaultActiveKey="0">
+          <Accordion.Item eventKey="0">
+            <Accordion.Header>Sobre o nosso sitema</Accordion.Header>
+            <Accordion.Body>
+              My Task é um sistema para gerenciamento de tarefas pessoais. Nesta
+              aplicação você pode gerenciar todos os seus afazeres.
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="1">
+            <Accordion.Header>Sobre a empresa</Accordion.Header>
+            <Accordion.Body>
+              Essa empresa desenvolve soluções focadas em produtividade e
+              bem-estar.
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+        <Button className="mt-2 me-2">Entre em contato</Button>
+        <Button variant="outline-danger" className="mt-2 me-2">
+          Denunciar
+        </Button>
+        <Alert variant="warning" className="mt-2">
+          Atenção! Estamos em manutenção.
+        </Alert>
+      </Container>
+    </main>
+  );
+}
+
+export default Ajuda;
