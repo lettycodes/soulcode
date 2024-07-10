56839ca218ea418d069d59843a8f3214b47679dd
lettycodes <sps.leticia@gmail.com>
2024-07-10 15:15:45 -0300

    Estilizando primeiras páginas com bootstrap

--- Etapa02/REACT/mytask/src/components/Menu.jsx
+++ Etapa02/REACT/mytask/src/components/Menu.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 // Link: este componente habilita o SPA (Single Page Application)
 // Obs.: se houver links externos, utilize a tag <a />
@@ -6,12 +7,33 @@ import { Link } from "react-router-dom";
 function Menu() {
   return (
     <header>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/login">Login</Link>
-        <Link to="/cadastro">Cadastro</Link>
-        <Link to="/ajuda">Ajuda</Link>
-      </nav>
+      <Navbar bg="dark" variant="dark" expand="sm">
+        <Container fluid>
+          <Link to="/">
+            <img
+              src="https://cdn.pixabay.com/photo/2017/09/29/00/30/checkmark-icon-2797531_640.png"
+              width="32"
+            />
+          </Link>
+          <Navbar.Toggle />
+          <Navbar.Collapse>
+            <Nav className="ms-auto">
+              <Link className="nav-link" to="/">
+                Home
+              </Link>
+              <Link className="nav-link" to="/login">
+                Login
+              </Link>
+              <Link className="nav-link" to="/cadastro">
+                Cadastro
+              </Link>
+              <Link className="nav-link" to="/ajuda">
+                Ajuda
+              </Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </header>
   );
 }
